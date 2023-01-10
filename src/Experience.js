65216579db50a70ea3369415d31a4563712f9cb7
Josiah Webb <jj.webb520@gmail.com>
2023-01-10 12:47:57 -0700

    adds links ref from boxGeo to transform controls

--- src/Experience.js
+++ src/Experience.js
@@ -1,28 +1,33 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, TransformControls } from '@react-three/drei'
+import { useRef } from 'react'
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+    const boxRef = useRef(null)
 
-        <OrbitControls />
+    return (     
+        <>
+            <OrbitControls />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+            <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+            <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <mesh position-x={ 2 } scale={ 1.5 } ref={ boxRef }>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+            <TransformControls object={ boxRef } /> 
+            {/* if using ref solution, this needs to be after the referenced obj */}
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+                <planeGeometry />
+                <meshStandardMaterial color="greenyellow" />
+            </mesh>
 
-    </>
+        </>
+    ) 
 }
