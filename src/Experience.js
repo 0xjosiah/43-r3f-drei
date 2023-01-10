d984e000a72b4ee259c80c45c439b6218f63957a
Josiah Webb <jj.webb520@gmail.com>
2023-01-10 13:08:04 -0700

    adds pivotControls to sphere (alt to transform), adds html element to sphere

--- src/Experience.js
+++ src/Experience.js
@@ -1,8 +1,9 @@
-import { OrbitControls, TransformControls } from '@react-three/drei'
+import { Html, OrbitControls, PivotControls, TransformControls } from '@react-three/drei'
 import { useRef } from 'react'
 
 export default function Experience() {
     const boxRef = useRef(null)
+    const sphere = useRef(null)
 
     return (     
         <>
@@ -11,22 +12,38 @@ export default function Experience() {
             <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
             <ambientLight intensity={ 0.5 } />
 
-            <mesh position-x={ - 2 }>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
+            <PivotControls 
+                anchor={[ 0, 0, 0 ]}
+                depthTest={ false }
+                lineWidth={ 4 }
+                axisColors={ [ 0x9381ff, 0xff4d6d, 0x7ae582 ]}
+                scale={ 2 }
+                // fixed={ true } this removes perspective, obj stays same size regardless of depth
+            >
+                <mesh position-x={ - 2 } ref={ sphere }>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="orange" />
+                    <Html
+                        position={[ 1, 1, 0 ]}
+                        wrapperClass="label"
+                    >
+                        Test, hello moto
+                    </Html>
+                </mesh>
+            </PivotControls>
 
             <mesh position-x={ 2 } scale={ 1.5 } ref={ boxRef }>
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
-            <TransformControls object={ boxRef } /> 
+            <TransformControls object={ boxRef } mode='translate' /> 
             {/* if using ref solution, this needs to be after the referenced obj, better solution because independent, can remove transform controls with no issue */}
 
             <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
             </mesh>
+
 
         </>
     ) 
