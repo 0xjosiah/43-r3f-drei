2d2c44e26a1839bf19993ad9cbedbfb5f04e0ae9
Josiah Webb <jj.webb520@gmail.com>
2023-01-10 12:37:27 -0700

    import and implement orbitControls with drei

--- src/Experience.js
+++ src/Experience.js
@@ -1,8 +1,10 @@
-// import
+import { OrbitControls } from '@react-three/drei'
 
 export default function Experience()
 {
     return <>
+
+        <OrbitControls />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
